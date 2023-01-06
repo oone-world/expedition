6412edae6fdc938f846f12fe182454a77ee69db1
nick-jones-adigium <nick.jones@adigium.com>
2023-01-06 11:50:28 +0200

    Make Oone World blockchain the main network

--- src/hooks/useChainList.ts
+++ src/hooks/useChainList.ts
@@ -5,22 +5,17 @@ import React from "react";
 
 // const mergeChainSets = (c1: Chain[], c2: Chain[]) => uniqBy(c1.concat(c2), "name");
 
-export default function() {
+export default function () {
   const [chains, setChains] = React.useState<Chain[]>([
+    {
+      name: "Oone.World",
+      network: "Oone",
+      rpc: ["https://adg.adigium.com"],
+    },
     {
       name: "Ethereum Classic",
       network: "mainnet",
       rpc: ["https://www.ethercluster.com/etc"],
-    },
-    {
-      name: "Matic",
-      network: "mainnet",
-      rpc: ["https://rpc-mainnet.matic.network"],
-    },
-    {
-      name: "Matic - Mumbai",
-      network: "testnet",
-      rpc: ["https://rpc-mumbai.matic.today"],
     },
   ]);
 
